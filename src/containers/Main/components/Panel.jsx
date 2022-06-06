62cbda7113ed097a23960a5eacf4a67e3c9d3292
Senir Sales Oliveira <senirsales@hotmail.com>
2022-06-06 10:43:23 -0300

    Update Panel.jsx

--- src/containers/Main/components/Panel.jsx
+++ src/containers/Main/components/Panel.jsx
@@ -29,7 +29,7 @@ function Panel({ updateAt, onChange, data, country, getCoviddata }) {
     navigator.share({
       title: `Dados do Covid19 - ${country}`,
       text: textCovid19,
-      url: 'https://covid19dio.netlify.app/'
+      url: 'https://covid19-dio-ss.netlify.app//'
     })
   }
 
@@ -53,8 +53,8 @@ function Panel({ updateAt, onChange, data, country, getCoviddata }) {
     <Card>
       <CardPanelContentStyled>
         <div>
-          <Typography variant="h5" component="span" color="primary">COVID19</Typography>
-          <Typography variant="h6" component="span" color="primary">Painel Coronavírus</Typography>
+          <Typography variant="h5" component="span" color="primary">COVID19</Typography><br />
+          <Typography variant="h6" component="span" color="primary">Painel Coronavírus</Typography><br />
           <Typography variant="body2" component="span" color="primary">Atualizado em: {updateAt}</Typography>
           <div className="pt-2">
             <Select onChange={onChange} value={country}>
